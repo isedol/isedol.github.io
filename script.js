19cab4710a3cc75d23a0ee9d697f263ea16807f4
isedol <dyghdh5@gmail.com>
2022-02-04 00:56:38 +0900

    asd

---
+++ script.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+      function filter(){
+
+        var value, name, item, i;
+
+        value = document.getElementById("value").value.toUpperCase();
+        item = document.getElementsByClassName("item");
+
+        for(i=0;i<item.length;i++){
+          name = item[i].getElementsByClassName("name");
+          if(name[0].innerHTML.toUpperCase().indexOf(value) > -1){
+            item[i].style.display = "flex";
+          }else{
+            item[i].style.display = "none";
+          }
+        }
+      }
+</script>
