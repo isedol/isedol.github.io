c68e48383d8caa2b21c3c7b5ce85cf00eb73bdd0
isedol <dyghdh5@gmail.com>
2022-02-05 07:34:22 +0900

    ㅎㄴㅇㅇㅇㄹ

--- README.md
+++ README.md
@@ -10,8 +10,6 @@ https://velog.io/@khw970421/%EC%98%A4%EB%94%94%EC%98%A4-%EB%A6%AC%EC%8A%A4%ED%8A
 img디자인 넣으려면 외부에서 땡기거나 무제한대역폭 가야됨 (현100gb)
 https://www.websiteplanet.com/ko/blog/%EB%85%84%EC%97%90-%EC%93%B0%EA%B8%B0-%EC%A2%8B%EC%9D%80-%EB%AC%B4%EB%A3%8C-%EC%9B%B9-%ED%98%B8%EC%8A%A4%ED%8C%85-%EC%84%9C%EB%B9%84%EC%8A%A4-%EB%B2%A0%EC%8A%A4%ED%8A%B8-5/
 
-곡div 분리
-https://beableto.tistory.com/m/entry/html-in-html-html%EC%97%90%EC%84%9C-html%EC%9D%84-%EB%B6%88%EB%9F%AC%EC%98%A4%EC%9E%90
 
 제이쿼리로 곡추가
 
--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function filter() {
+$(document).ready( function() {
+  //페이지가 로드되면 실행한다.
+  filter2('고');
 
+  //세구를 기본
+
+});
+
+function filter() {
+    //검색
     var value, name, item, i;
 
     value = document.getElementById("value").value.toUpperCase();
@@ -16,20 +24,32 @@ function filter() {
   }
 
 
+  // function filter2(c) {
+
+  //   var item, i, icon;
+
+  //   item = document.getElementsByClassName("item");
+
+  //   if (c == "모두") c = "";
+
+  //   for (i = 0; i < item.length; i++) {
+  //     icon = item[i].getElementsByClassName("icon");
+  //     if (icon[0].innerHTML.toUpperCase().indexOf(c) > -1) {
+  //       item[i].style.display = "flex";
+  //     } else {
+  //       item[i].style.display = "none";
+  //     }
+  //   }
+  // }
+ 
   function filter2(c) {
+      //필터
+      var c;
 
-    var item, i, icon;
-
-    item = document.getElementsByClassName("item");
-
-    if (c == "모두") c = "";
-
-    for (i = 0; i < item.length; i++) {
-      icon = item[i].getElementsByClassName("icon");
-      if (icon[0].innerHTML.toUpperCase().indexOf(c) > -1) {
-        item[i].style.display = "flex";
-      } else {
-        item[i].style.display = "none";
-      }
-    }
+      if(c=='고') $(".container").load("gosegu.html");
+      else if(c=='릴') $(".container").load("lilpa.html");
+      else if(c=='아') $(".container").load("ine.html");
+      else if(c=='징') $(".container").load("jing.html");
+      else if(c=='챤') $(".container").load("vichan.html");
   }
+  
