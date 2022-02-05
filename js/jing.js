6d78bbda0d861262f0a810f1ff8f437995e96000
isedol <dyghdh5@gmail.com>
2022-02-05 11:28:27 +0900

    asdasd

---
+++ js/jing.js
@@ -0,0 +1,31 @@
+
+// test image for web notifications
+var iconImage = null;
+
+AP.init({
+    container: '#player',//a string containing one CSS selector
+    volume: 0.7,
+    autoPlay: true,
+    notification: false,
+    playList: [
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+    ]
+});
