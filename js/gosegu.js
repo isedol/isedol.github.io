3c3890850f469b263ee09a377e77c2145b7d6fd8
isedol <dyghdh5@gmail.com>
2022-02-05 10:21:09 +0900

    asdasd

---
+++ js/gosegu.js
@@ -0,0 +1,36 @@
+
+// test image for web notifications
+var iconImage = null;
+
+AP.init({
+    container: '#player',//a string containing one CSS selector
+    volume: 0.7,
+    autoPlay: true,
+    notification: false,
+    playList: [
+        { 'icon': iconImage, 'title': '10cm - 사랑은 은하수 다방에서', 'file': 'https://docs.google.com/uc?export=open&id=169y-7jBhiPxgpEWg6tY3OAAa7Oo6IrDB' },
+        { 'icon': iconImage, 'title': '10cm - 폰서트', 'file': 'https://docs.google.com/uc?export=open&id=1Nva5xZnqevtUJuep7v2hszYk3AZKziEM' },
+        { 'icon': iconImage, 'title': '출근송', 'file': 'https://docs.google.com/uc?export=open&id=15nAw_VWHegObNhp1m7yq7YUkeJJhDJ4K' },
+        { 'icon': iconImage, 'title': '넬 - 기억을 걷는시간', 'file': 'https://docs.google.com/uc?export=open&id=1_ziWoG0DeDsRlyDe07HUPzHA6fyv83ML' },
+        { 'icon': iconImage, 'title': '백예린 - 이럴거면 그러지 말지', 'file': 'https://docs.google.com/uc?export=open&id=12TVzzXemHvLz6UX5YwPvbI1hBMoLesb6' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+        { 'icon': iconImage, 'title': '', 'file': '' },
+    ]
+});
