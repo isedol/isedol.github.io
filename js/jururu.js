2e19a5ce426f16c2594f4594006b07e149118b71
isedol <dyghdh5@gmail.com>
2022-02-27 16:45:36 +0900

    asd

---
+++ js/jururu.js
@@ -0,0 +1,14 @@
+
+// test image for web notifications
+var iconImage = null;
+
+AP.init({
+    container: '#player',//a string containing one CSS selector
+    volume: 0.7,
+    autoPlay: false,
+    notification: false,
+    playList: [
+         
+        
+    ]
+});
